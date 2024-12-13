f68083bdec999018872996c2514c55930d5597f7
Kirandinesh <kirandinesh646@gmail.com>
2024-12-13 12:23:11 +0530

    new

--- client/src/components/Shop-view/ShopMenu/ShopMenu.jsx
+++ client/src/components/Shop-view/ShopMenu/ShopMenu.jsx
@@ -39,7 +39,7 @@ function ShopMenu() {
   }, [dispatch]);
 
   useEffect(() => {
-    const savedActiveMenuIndex = localStorage.getItem("activeMenuIndex");
+    const savedActiveMenuIndex = sessionStorage.getItem("activeMenuIndex");
     if (savedActiveMenuIndex) {
       setActiveMenuIndex(parseInt(savedActiveMenuIndex, 10));
     }
@@ -47,7 +47,7 @@ function ShopMenu() {
 
   useEffect(() => {
     if (activeMenuIndex !== null) {
-      localStorage.setItem("activeMenuIndex", activeMenuIndex);
+      sessionStorage.setItem("activeMenuIndex", activeMenuIndex);
     }
   }, [activeMenuIndex]);
 
