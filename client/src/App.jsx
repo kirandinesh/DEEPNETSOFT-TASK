edc663a3997e400abf8b02022a2a5964238f0c03
Kirandinesh <kirandinesh646@gmail.com>
2024-12-13 11:48:12 +0530

    new

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,13 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import ShopLayout from "./components/Shop-view/ShopLayout/ShopLayout";
 
 function App() {
   return (
     <div className="App w-screen h-screen bg-black overflow-hidden">
-      <ShopLayout />
+      <Routes>
+        <Route path="/" element={<ShopLayout />} />
+      </Routes>
     </div>
   );
 }
