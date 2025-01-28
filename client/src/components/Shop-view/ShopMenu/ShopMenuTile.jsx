28e7d1dfeb018e426d4c3f00334bab8c6aad3a20
Kirandinesh <kirandinesh646@gmail.com>
2025-01-28 16:37:28 +0530

    newnew

--- client/src/components/Shop-view/ShopMenu/ShopMenuTile.jsx
+++ client/src/components/Shop-view/ShopMenu/ShopMenuTile.jsx
@@ -1,29 +1,18 @@
 function ShopMenuTile({ name, description, price }) {
   return (
-    <div className="w-full h-[66px] flex flex-col ">
-      <div className="flex items-end ">
-        <span
-          className="font-title text-base lg:text-[26px]  pr-1
-        
-        "
-        >
+    <div className="w-full h-auto flex flex-col  p-4 transition-shadow duration-300 ">
+      <div className="flex items-end">
+        <span className="font-title text-lg md:text-xl lg:text-2xl text-white ">
           {name}
         </span>
 
-        <div
-          className="flex-grow border-b-2 mb-1 border-dotted
-        border-white lg:mb-0 lg:border-b-4"
-        ></div>
+        <div className="flex-grow border-b-2 border-dotted border-gray-400 dark:border-gray-600 mb-1 md:mb-0"></div>
 
-        <span className="pl-2  font-title text-base lg:text-[26px]">
+        <span className="pl-2 font-title text-lg md:text-xl lg:text-2xl ">
           ${price}
         </span>
       </div>
-      <div
-        className="font-par text-[14px] text-[#857878] mt-1
-      lg:text-[18px]
-      "
-      >
+      <div className="font-par text-sm md:text-base lg:text-lg  mt-2 opacity-75">
         {description}
       </div>
     </div>
--- client/src/page/items-content/index.jsx
+++ client/src/page/items-content/index.jsx
@@ -5,7 +5,7 @@ function MenuItemList({ items, index, menuList }) {
   console.log(items);
   return (
     <div
-      className="py-3  lg:px-10 lg:py-4 lg:grid lg:grid-cols-2
+      className="py-3 flex flex-col gap-2 lg:px-10 lg:py-4 lg:grid lg:grid-cols-2
     lg:gap-x-14  lg:gap-y-8 "
     >
       {items.map((item, index) => (
